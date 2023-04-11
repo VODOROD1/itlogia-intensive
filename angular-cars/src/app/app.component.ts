bef606305de3252acc2d6abc6791599785deb291
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-04-11 17:06:39 +0400

    formBuilder was added

--- angular-cars/src/app/app.component.ts
+++ angular-cars/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,31 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cars';
+  priceForm: FormGroup = this.fb.group({
+    name: ['', Validators.required],
+    telephone: ['', Validators.required],
+    car: ['', Validators.required],
+  });
 
-  @ViewChild('priceAction', { static: false })
-  public priceAction: ElementRef;
-
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   goScroll(target: HTMLElement) {
     target.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  priceActionHandler() {
+    if(this.priceForm.valid) {
+      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+    }
+
+    if (this.priceForm.get('name')?.value === '') {
+      alert('Заполните поле имя!');
+    }
+    if (this.priceForm.get('telephone')?.value === '') {
+      alert('Заполните поле номер телефона!');
+    }
+    if (this.priceForm.get('car')?.value === '') {
+      alert('Заполните поле желаемый автомобиль!');
+    }
   }
 }
