63ae627925340c69dec946846b201d064ad552c8
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-04-11 15:41:43 +0400

    project files from cars were added in angular-cars projetc

--- angular-cars/src/app/app.component.ts
+++ angular-cars/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'angular-cars';
+
+  @ViewChild('priceAction', {static: false})
+  public priceAction: ElementRef
+
+  constructor() {
+    
+  }
+
+  goScroll(target: HTMLElement) {
+    target.scrollIntoView({behavior: "smooth"})
+  }
 }
