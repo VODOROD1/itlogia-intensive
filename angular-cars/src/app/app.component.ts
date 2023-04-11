81e4043ab378901ef979ed456a4febd7d08fec24
Mike_Belyakov <48880903+VODOROD1@users.noreply.github.com>
2023-04-11 19:25:16 +0400

    parallax-effect from origin script.js file was introduced in angular-cars

--- angular-cars/src/app/app.component.ts
+++ angular-cars/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface ICar {
-  image: string,
-  name: string,
-  transmission: string,
-  engine: string,
-  year: string
+  image: string;
+  name: string;
+  transmission: string;
+  engine: string;
+  year: string;
 }
 
 @Component({
@@ -28,75 +28,96 @@ export class AppComponent {
       name: 'Lamborghini Huracan Spyder',
       transmission: 'автомат',
       engine: '5.2 л.с.',
-      year: '2019'
+      year: '2019',
     },
     {
       image: '2.png',
       name: 'Chevrolet Corvette',
       transmission: 'автомат',
       engine: '6.2 л.с.',
-      year: '2017'
+      year: '2017',
     },
     {
       image: '3.png',
       name: 'Ferrari California',
       transmission: 'автомат',
       engine: '3.9 л.с.',
-      year: '2010'
+      year: '2010',
     },
     {
       image: '4.png',
       name: 'Lamborghini Urus',
       transmission: 'автомат',
       engine: '4.0 л.с.',
-      year: '2019'
+      year: '2019',
     },
     {
       image: '5.png',
       name: 'Audi R8',
       transmission: 'автомат',
       engine: '5.2 л.с.',
-      year: '2018'
+      year: '2018',
     },
     {
       image: '6.png',
       name: 'Chevrolet Camaro',
       transmission: 'автомат',
       engine: '2.0 л.с.',
-      year: '2019'
+      year: '2019',
     },
     {
       image: '7.png',
       name: 'Maserati Quattroporte',
       transmission: 'автомат',
       engine: '3.0 л.с.',
-      year: '2018'
+      year: '2018',
     },
     {
       image: '8.png',
       name: 'Dodge Challenger',
       transmission: 'автомат',
       engine: '6.4 л.с.',
-      year: '2019'
+      year: '2019',
     },
     {
       image: '9.png',
       name: 'Nissan GT-R',
       transmission: 'автомат',
       engine: '3.8 л.с.',
-      year: '2019'
-    }
+      year: '2019',
+    },
   ];
 
-  constructor(private fb: FormBuilder) {}
+  @ViewChild('main', {static: false})
+  mainRef: ElementRef;
+
+  @ViewChild('priceImage', {static: false})
+  priceImage: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    document.addEventListener('DOMContentLoaded', () => {
+      document.addEventListener('scroll', () => {
+        this.mainRef.nativeElement.style.backgroundPositionX = '0' + 0.3 * window.pageYOffset + 'px';
+      });
+
+      document.addEventListener('mousemove', (event) => {
+        this.priceImage.nativeElement.style.transform =
+          'translate3d(' +
+          (event.clientX * 0.4) / 8 +
+          'px,' +
+          (event.clientY * 0.4) / 8 +
+          'px,0px)';
+      });
+    });
+  }
 
   goScroll(target: HTMLElement) {
     target.scrollIntoView({ behavior: 'smooth' });
   }
 
   priceActionHandler() {
-    if(this.priceForm.valid) {
-      alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!")
+    if (this.priceForm.valid) {
+      alert('Спасибо за заявку, мы свяжемся с вами в ближайшее время!');
     }
 
     if (this.priceForm.get('name')?.value === '') {
